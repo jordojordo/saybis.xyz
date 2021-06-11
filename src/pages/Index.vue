21734825a2533bc4a9054b1d38ff79fb9a7561f3
jordonleach@gmail.com <jordonleach@gmail.com>
2021-06-11 18:22:33 -0400

    add init proxy form

--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -1,20 +1,56 @@
 <template>
   <Layout>
+    <form id="ProxyRequest" name="ProxyRequest" @submit.prevent="handleSubmit">
+      <label for="RequestTarget">
+        <input
+          name="RequestTarget"
+          id="RequestTarget"
+          v-model="RequestTarget"
+          type="text"
+          placeholder="example.com"
+          required
+        />
+      </label>
 
-    <h1>my dude</h1>
-
-    <p>
-      ingress is the answer
-    </p>
-
+      <label>
+        <button>
+          Submit
+        </button>
+      </label>
+    </form>
   </Layout>
 </template>
 
 <script>
+import axios from "axios";
+
 export default {
   metaInfo: {
-    title: 'devs are dum'
-  }
-}
+    title: "devs are dum",
+  },
+  data() {
+    return {
+      RequestTarget: "",
+    };
+  },
+  methods: {
+    handleSubmit() {
+      axios
+        .post("https://saybis-node.default.45.79.180.102.xip.io", this.RequestTarget, {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+          },
+        })
+        .then((res) => {
+          console.log(res);
+          // this.form = {
+          //   RequestTarget: "",
+          // };
+        })
+        .catch((error) => {
+          console.error(error);
+        });
+    },
+  },
+};
 </script>
-
